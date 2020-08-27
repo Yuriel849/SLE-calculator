45db43eacde6fcd79442ad04b9b83614fc57af80
Yuriel849 <yuriel849sp@gmail.com>
2020-08-27 17:44:27 +0200

    setValue, getValue, setSize, getSize methods added

--- SLE-Calculator Java/src/de/haw/common/Matrix.java
+++ SLE-Calculator Java/src/de/haw/common/Matrix.java
@@ -3,15 +3,62 @@ package de.haw.common;
 import java.util.ArrayList;
 
 public class Matrix {
-	public ArrayList<ArrayList<Double>> rows;
-	int rowSize;
+	private ArrayList<ArrayList<Double>> rows;
+	private int rowSize;
 	
 	public Matrix(int rowSize) {
 		this.rowSize = rowSize;
 		rows = new ArrayList<>();
 		
 		for(int i = 0; i < rowSize; i++) {
-			rows.add(new ArrayList<>());
+			rows.add(new ArrayList<>(rowSize+1));
 		}
+	}
+	
+	public boolean setValue(int row, int column, double value) {
+		boolean result = false;
+		
+		if(0 <= row && row < rowSize && 0 <= column && column <= rowSize)
+			rows.get(row).set(column, value);
+		else
+			System.out.println("Error: Row or column number is invalid.");
+		
+		return result;		
+	}
+	
+	public double getValue(int row, int column) {
+		double result = -9999;
+		
+		if(0 <= row && row < rowSize && 0 <= column && column <= rowSize)
+			result = rows.get(row).get(column);
+		else
+			System.out.println("Error: Row or column number is invalid.");
+		
+		return result;
+	}
+	
+	public boolean setSize(int size) {
+		boolean result = false;
+		
+		if(rowSize > size) {
+			for(int i = rowSize; i > size; i--) {
+				rows.remove(i - 1);
+			}
+			rowSize = size;
+			result = true;
+		}
+		else if (rowSize < size) {
+			for(int i = rowSize; i < size; i++) {
+				rows.add(new ArrayList<>(size+1));
+			}
+			rowSize = size;
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	public int getSize() {
+		return rowSize;
 	}
 }
