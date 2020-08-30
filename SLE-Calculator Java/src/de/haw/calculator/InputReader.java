5640166e7d90935da64ca42978d1f861e93bef2f
DESKTOP-6LOHJQN\m <mario.ivanov@pm.me>
2020-08-30 11:27:59 +0200

    Work in progress. Added more logical checks to Input Reader and solver.

--- SLE-Calculator Java/src/de/haw/calculator/InputReader.java
+++ SLE-Calculator Java/src/de/haw/calculator/InputReader.java
@@ -10,61 +10,74 @@ import de.haw.common.*;
  * @author test
  *
  */
-public class InputReader 
+public class InputReader
 {
 	private Scanner scan;
-	
+
 	public InputReader()
 	{
 		scan = new Scanner(System.in);
 	}
-	
+
 	public Matrix getSystem()
 	{
-		boolean jnz =false;
+		boolean jnz = false;
 		Integer rows = 0;
 		String str = new String();
 		Matrix mat;
 		boolean entered = false;
 		int i = 0;
 		int j = 0;
-			// Retrieves number of rows from the user input
-	    	while(!jnz) {
-	    		System.out.println("Enter number of equations > ");
-	    		str = scan.nextLine().trim();
-	    		rows = Integer.parseInt(str);
-	    		if(rows > 1){
-	    			jnz = true;
-	    		}
-	    		else {
-	    			System.out.println("Must be positive integer greater than 1");
-	    		}
-	    	}
-	    mat = new Matrix((int)rows);
-	    	// Retrieves values of equation coefficients
-	    	while(!entered) {
-	    		System.out.println("Enter the coeficients of the equation on a single line separated by space and press enter");
-	    		System.out.print("> ");
-	    		str = scan.nextLine().trim();
-	    		String[] tokens = str.split(" ");
-	    		 if(tokens.length != (rows + 1)) {
-	    		 	 System.out.println("Try again:");
-	    		 }
-	    		 else {
-	    			 for(String s : tokens) {
-	    				 j %=(rows + 1);
-	    				 mat.setValue(i,j ,Double.parseDouble(s));
-	    				 j++;
-	    			 }
-	    			 i++;
-	    			 	if(i == rows) {
-	    			 		entered = true;
-	    			 	}
-	    		 }
-	    	}
+		// Retrieves number of rows from the user input
+		while (!jnz) {
+			System.out.println("Enter number of equations > ");
+			str = scan.nextLine().trim();
+			rows = Integer.parseInt(str);
+			if (rows > 1) {
+				jnz = true;
+			}
+			else {
+				System.out.println("Must be positive integer greater than 1");
+			}
+		}
+		mat = new Matrix((int) rows);
+		// Retrieves values of equation coefficients
+		while (!entered) {
+			System.out.println(
+					"Enter the coeficients of the equation on a single line separated by space and press enter");
+			System.out.print("> ");
+			str = scan.nextLine().trim();
+			String[] tokens = str.split(" ");
+			boolean sEntered = false;
+			if (tokens.length != (rows + 1)) {
+				System.out.println("Try again:");
+			}
+			else {
+				for (String s : tokens) {
+					j %= (rows + 1);
+					try {
+						//@SuppressWarnings("unused")
+						Double.parseDouble(s);
+						sEntered = true;
+					}
+					catch (Exception e) {
+						System.out.println("Not a number!");
+						sEntered = false;
+						break;
+					}
+					mat.setValue(i, j, Double.parseDouble(s));
+					j++;
+				}
+				if (sEntered) {
+					i++;
+				}
+				if (i == rows) {
+					entered = true;
+				}
+			}
+		}
 		
 		return mat;
 	}
 
 }
-	
