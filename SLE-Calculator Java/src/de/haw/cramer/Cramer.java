b112725c9dbc4b4913390eb913ad9eed8dcc6446
Yuriel849 <yuriel849sp@gmail.com>
2020-09-02 19:46:40 +0200

    New class to implement Cramer's Rule

---
+++ SLE-Calculator Java/src/de/haw/cramer/Cramer.java
@@ -0,0 +1,20 @@
+package de.haw.cramer;
+
+import de.haw.common.Solveable;
+
+/**
+ * Solves a system of linear equations using Cramer's Rule.
+ * 
+ * @author Yuriel
+ * @version 02.09.2020.
+ */
+public class Cramer implements Solveable {
+
+	@Override
+	public void solveSystem() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
